52fcf299a3a774ce61123aaf6f470445f17925e0
vagrant <vagrant@vagrant.vm>
2019-03-11 13:15:20 +0000

    modify query to include top 3 results

--- newsreport.py
+++ newsreport.py
@@ -14,34 +14,17 @@ def get_connection():
     return _connection
 
 
-def get_total_200_logs():
-	db = get_connection()
-	c = db.cursor()
-	c.execute("select path, count(*) as num_200 from log where status = '200 OK' group by path order by num_200 asc")
-	rows = c.fetchall()
-	return rows
-
 def strip_path():
 	db = get_connection()
 	c = db.cursor()
 
-	#get slug from path
-
-	c.execute("select title, count(slug) from (select split_part(path, '/article/', 2) as log_slug from log) as log_slug_table, articles where log_slug = articles.slug group by title")
+	#get slug from path of log table and compare with the articles table slug, then get log counts for titles
+	c.execute("select title, count(slug) from (select split_part(path, '/article/', 2) as log_slug from log) as log_slug_table, articles where log_slug = articles.slug group by title order by count(slug) desc limit 3")
 	
 	rows = c.fetchall()
 	for row in rows:
 		print(row[0],row[1])
-	# for row in rows:
-	# 	if len(row[0]) > 1:
-	# 		slugs.append(row[0].partition('/article')[2])
 
-	# for slug in slugs:
-	# 	print(slug)
-
-
-for  row in get_total_200_logs():
-	print(row)
 
 strip_path()
 
