0d00ad6d2b68c21c6bd1019dfd643e87852e7a09
vagrant <vagrant@vagrant.vm>
2019-03-12 15:18:23 +0000

    started implementation of get_high_error_days(), query counts all the 404 events and aggregates them over date

--- newsreport.py
+++ newsreport.py
@@ -18,7 +18,7 @@ def get_top_three_articles():
 	db = get_connection()
 	c = db.cursor()
 
-	#get slug from path of log table and compare with the articles table slug, then get log counts for titles
+	#match path to slug by extending slug with "/article/" string, then get log counts for titles, store aggregation and join in a view for reuse
 	c.execute("""
 		create view popular_articles as 
 		select count(log.id) as views, title, author 
@@ -39,14 +39,34 @@ def get_top_authors():
 	c = db.cursor()
 
 	# use popular_articles view to join with authors table and aggregate the article views for each author
-	c.execute("select sum(views), authors.name from popular_articles, authors where popular_articles.author = authors.id group by name order by count(views) desc")
+	c.execute("""
+		select sum(views), authors.name 
+			from popular_articles, authors 
+		where popular_articles.author = authors.id 
+			group by name 
+			order by count(views) desc""")
 
 	rows = c.fetchall()
 	for row in rows:
 		print(row[0], row[1])
 
+def get_high_error_days():
+	db = get_connection()
+	c = db.cursor()
+
+	c.execute("""
+		select count(date(time)), date(time)
+			from log 
+			where status like '4%'
+			group by date(time)
+			order by count(date(time))""")
+
+	rows = c.fetchall()
+	for row in rows:
+		print(row[1], row[0])
 
 get_top_three_articles()
 get_top_authors()
+get_high_error_days()
 
 _connection.close()
