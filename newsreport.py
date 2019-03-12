683be788e2a14bee5cde6d18774c0d3683a7250c
vagrant <vagrant@vagrant.vm>
2019-03-12 13:51:57 +0000

    altered the top article query to temporarly store article-log relation in a view for using it in next queries

--- newsreport.py
+++ newsreport.py
@@ -14,27 +14,19 @@ def get_connection():
     return _connection
 
 
-def strip_path():
+def get_top_three_articles():
 	db = get_connection()
 	c = db.cursor()
 
 	#get slug from path of log table and compare with the articles table slug, then get log counts for titles
-	c.execute("select title, count(slug) from (select split_part(path, '/article/', 2) as log_slug from log) as log_slug_table, articles where log_slug = articles.slug group by title order by count(slug) desc limit 3")
-	
+	c.execute("create view popular_articles as select count(slug), title from (select split_part(path, '/article/', 2) as log_slug from log) as log_slug_table, articles where log_slug = articles.slug group by title order by count(slug) desc")
+	c.execute("select * from popular_articles limit 3")
+
 	rows = c.fetchall()
+
 	for row in rows:
 		print(row[0],row[1])
 
-
-strip_path()
+get_top_three_articles()
 
 _connection.close()
-
-
-# def add_post(content):
-# 	db = psycopg2.connect(database = DBNAME)
-# 	c = db.cursor()
-# 	content = bleach.clean(content)
-# 	c.execute("insert into posts values (%s)", (content,))
-# 	db.commit()
-# 	db.close()
