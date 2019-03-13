ebf987d10b1b4a5f0e15c73116bee0424fa9479c
vagrant <vagrant@vagrant.vm>
2019-03-13 09:28:24 +0000

    implemented def get_high_error_days():

--- newsreport.py
+++ newsreport.py
@@ -55,15 +55,28 @@ def get_high_error_days():
 	c = db.cursor()
 
 	c.execute("""
-		select count(date(time)), date(time)
-			from log 
+		select error_per_date_view.error_logs as error, total_date
+			from
+		(
+		select count(date(time)) as total_logs, date(time) as total_date
+					from log 
+				group by date(time)
+				order by date(time)
+		) as total_log_view, 
+		(
+			select count(date(time)) as error_logs, date(time) as error_date
+				from log 
 			where status like '4%'
-			group by date(time)
-			order by count(date(time))""")
+				group by date(time)
+				order by count(date(time))
+		) as error_per_date_view
+		where total_date = error_date and  ((error_per_date_view.error_logs * 100)::numeric/total_log_view.total_logs) > 1
+			group by error, total_date
+		""")
 
 	rows = c.fetchall()
 	for row in rows:
-		print(row[1], row[0])
+		print(row[0], row[1])
 
 get_top_three_articles()
 get_top_authors()
